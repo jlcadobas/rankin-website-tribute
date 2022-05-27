91e50f0c7436b81da6886817808c7f3da3a1c444
unknown <johnloydcorletadobas@gmail.com>
2022-05-27 12:56:48 +0800

    change pixels to width

---
+++ scripts/knotsAndCrosses.js
@@ -0,0 +1,9 @@
+
+    function knotsAndCrosses() {
+
+        var content = "nani";
+        
+        window.alert(content);
+    }
+
+    document.getElementById("nani").addEventListener("click", knotsAndCrosses);
